8a5cdeba22d550ec907fbc6e0cfe6403fbe1ffca
yiska <yiska0556678570@gmail.com>
2024-06-10 14:33:29 +0300

    cp2 finish

--- trProject/my_client.c
+++ trProject/my_client.c
@@ -10,47 +10,62 @@
 
 #define PORT 8080
 #define MAXLINE 1024
+#define MAXLINEINPUT 65
 
-// Driver code
-int main()
-{
-	int sockfd;
-	char buffer[MAXLINE];
-	char input[MAXLINE];
-	const char *hello = "Hello from client";
-	struct sockaddr_in servaddr;
+void clear_input_buffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) {
+        // Discard remaining characters
+    }
+}
 
-	// Creating socket file descriptor
-	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-		perror("socket creation failed");
-		exit(EXIT_FAILURE);
-	}
+int main() {
+    int sockfd;
+    char buffer[MAXLINE];
+    char input[MAXLINEINPUT + 1];  // +1 for the null terminator
+    struct sockaddr_in servaddr;
 
-	memset(&servaddr, 0, sizeof(servaddr));
+    // Creating socket file descriptor
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
 
-	// Filling server information
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(PORT);
-	servaddr.sin_addr.s_addr = INADDR_ANY;
+    memset(&servaddr, 0, sizeof(servaddr));
 
-	int n;
-	socklen_t len;
-	// Read input from user
-	while (true) {
-		printf("Enter message: ");
-		fgets(input, MAXLINE, stdin);
-		input[strcspn(input, "\n")] = 0; // Remove newline character
+    // Filling server information
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
 
-		// Send message to server
-		sendto(sockfd, (const char *)input, strlen(input), MSG_CONFIRM, (const struct sockaddr *)&servaddr,
-		       sizeof(servaddr));
-		printf("Message sent: %s\n", input);
+    int n;
+    socklen_t  len;
 
-		// Receive message from server
-		n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *)&servaddr, &len);
-		buffer[n] = '\0';
-		printf("Server: %s\n", buffer);
-	}
-	close(sockfd);
-	return 0;
+    while (true) {
+        printf("Enter message: ");
+        if (fgets(input, sizeof(input), stdin) != NULL) {
+            size_t l = strlen(input);
+            if (l > 0 && input[l - 1] == '\n') {
+                input[l - 1] = '\0';
+                l--;
+
+            }
+
+            // Clear input buffer if the input is too long
+            if (l == MAXLINEINPUT) {
+                clear_input_buffer();
+            }
+
+            // Send message to server
+            sendto(sockfd, (const char *)input, strlen(input), MSG_CONFIRM, (const struct sockaddr *)&servaddr, sizeof(servaddr));
+
+            // Receive message from server
+            n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *)&servaddr, &len);
+            buffer[n] = '\0';
+            printf("Server: %s\n", buffer);
+        }
+    }
+
+    close(sockfd);
+    return 0;
 }
--- trProject/my_server.c
+++ trProject/my_server.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -11,8 +10,12 @@
 
 #define PORT 8080
 #define MAXLINE 1024
+#define MAX_LINE_LENGTH 64
 
 void sent_msg(const char *msg);
+int count_number_of_lines(const char *filename);
+void info_comand(const char *filename, int num_line);
+void log_comand(const char *filename, char *buffer);
 
 socklen_t len;
 int sockfd;
@@ -21,7 +24,10 @@ struct sockaddr_in servaddr, cliaddr;
 int main()
 {
 	char buffer[MAXLINE];
-
+	const char *filename = "log.txt";
+	int len_msg_recive;
+	char command[4];
+	int num_line;
 	// Creating socket file descriptor
 	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
 		perror("socket creation failed");
@@ -42,48 +48,158 @@ int main()
 		exit(EXIT_FAILURE);
 	}
 
-	int n;
-
 	len = sizeof(cliaddr); //len is value/result
 	while (true) {
-		n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *)&cliaddr, &len);
-		buffer[n] = '\0';
-		printf("Client : %s\n", buffer);
-		if (strncmp(buffer, "log ", 4) == 0) {
-			printf("Log message: %s\n", buffer + 4);
-			FILE *fp = fopen("log.txt", "a");
-			if (fp == NULL) {
-				perror("Error opening file");
-				exit(EXIT_FAILURE);
-			}
-			fwrite(buffer + 4, 1, strlen(buffer) - 4, fp);
-			fputc('\n', fp);
-			fclose(fp);
+		len_msg_recive = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *)&cliaddr, &len);
+		buffer[len_msg_recive] = '\0';
+		if (len_msg_recive>64 ){
+			sent_msg("Message length should be less than or equal to 64 bytes\n");
+			continue;
 		}
-		elif (strncmp(buffer, "info ", 5) == 0)
-		{
-			char command[4];
-			int n;
-			if (sscanf(buffer, "%s %d", command, &n) == 2) {
-				if (n <= 8) {
-					//give n line
-				} else {
-					const char *msg = "The number must be positive and less than or equal to 8.\n";
-					sent_msg(msg);
-				}
-			} else {
-				const char *msg =
-					"There are two possible actions:\n1. log: in the format of log .....\n2. info: in the format of info n (n is number between 0-8)\n";
+		printf("Client:%s\n", buffer);
+		if (strncmp(buffer, "log ", 4) == 0) {
+			log_comand(filename, buffer);
+		} else if (strncmp(buffer, "info ", 5) == 0 && sscanf(buffer, "%s %d", command, &num_line) == 2) {
+			info_comand(filename, num_line);
+			continue;
+		} else {
+			sent_msg(
+				"There are two possible actions:\n1. log: in the format of log .....\n2. info: in the format of info n (n is number between 0-8)\n");
+		}
+	}
+	return 0;
+}
+void sent_msg(const char *msg)
+{
+	sendto(sockfd, msg, strlen(msg), MSG_CONFIRM, (const struct sockaddr *)&cliaddr, len);
+	printf("message sent.\n");
+}
+int count_number_of_lines(const char *filename)
+{
+	FILE *fp;
+	int count = 0; // Line counter (result)
+	char c; // To store a character read from file
+	// Open the file
+	fp = fopen(filename, "r");
+	// Check if file exists
+	if (fp == NULL) {
+		printf("Could not open file %s", filename);
+		return -1;
+	}
+
+	// Extract characters from file and store in character c
+	for (c = getc(fp); c != EOF; c = getc(fp))
+		if (c == '\n') // Increment count if this character is newline
+			count = count + 1;
+	// Close the file
+	fclose(fp);
+	printf("The file %s has %d lines\n ", filename, count);
+	return count;
+}
+void log_comand(const char *filename, char *buffer)
+{
+	printf("Log message:%s\n", buffer + 4);
+	FILE *fp = fopen(filename, "a");
+	if (fp == NULL) {
+		sent_msg("Error opening file\n");
+		return;
+	}
+	fwrite(buffer + 4, 1, strlen(buffer) - 4, fp);
+	fputc('\n', fp);
+	fclose(fp);
+	sent_msg("Log message received\n");
+}
+/*void info_comand(const char *filename, int num_line)
+{
+	char msg[MAXLINE] = "Info message recive\n";
+
+	if (num_line <= 8) {
+		int total_lines = count_number_of_lines(filename);
+		if (total_lines == -1) {
+			strcpy(msg, "Error opening file\n");
+			sent_msg(msg);
+			return;
+		}
+		printf("1");
+		if (num_line > total_lines) {
+			printf("2");
+			snprintf(msg, MAXLINE, "Have only %d logs\n", total_lines);
+			sent_msg(msg);
+			return;
+		} else {
+			printf("3");
+			FILE *file = fopen(filename, "r");
+			if (file == NULL) {
+				strcpy(msg, "Error opening file\n");
 				sent_msg(msg);
+				return;
 			}
 
-			//sendto(sockfd, (const char *)hello, strlen(hello), MSG_CONFIRM, (const struct sockaddr *)&cliaddr, len);
-			//printf("message sent.\n");
+			int start_line = total_lines - num_line;
+			int current_line = 0;
+			char buffer[MAX_LINE_LENGTH];
+
+			while (fgets(buffer, MAX_LINE_LENGTH, file)) {
+				if (current_line >= start_line) {
+					if (current_line == start_line) {
+						msg[0] = '\0';
+					}
+					strncat(msg, buffer, MAX_LINE_LENGTH - strlen(msg) - 1);
+
+				}
+				current_line++;
+			}
+
+			fclose(file);
 		}
-		return 0;
 	}
-	void sent_msg(const char *msg)
-	{
-		sendto(sockfd, msg, strlen(msg), MSG_CONFIRM, (const struct sockaddr *)&cliaddr, len);
-		printf("message sent.\n");
+	else{
+		sent_msg("number between 0-8\n");
 	}
+	
+}*/
+void info_comand(const char *filename, int num_line)
+{
+    char msg[MAXLINE] = "Info message received\n";
+
+    if (num_line <= 8) {
+        int total_lines = count_number_of_lines(filename);
+        if (total_lines == -1) {
+            strcpy(msg, "Error opening file\n");
+            sent_msg(msg);
+            return;
+        }
+        if (num_line > total_lines) {
+            snprintf(msg, MAXLINE, "Have only %d logs\n", total_lines);
+            sent_msg(msg);
+            return;
+        } else {
+            FILE *file = fopen(filename, "r");
+            if (file == NULL) {
+                strcpy(msg, "Error opening file\n");
+                sent_msg(msg);
+                return;
+            }
+
+            int start_line = total_lines - num_line;
+            int current_line = 0;
+            char buffer[MAX_LINE_LENGTH];
+
+            // Reset msg to store the info message
+            msg[0] = '\0';
+
+            while (fgets(buffer, MAX_LINE_LENGTH, file)) {
+                if (current_line >= start_line) {
+                    strncat(msg, buffer, MAXLINE - strlen(msg) - 1);
+                }
+                current_line++;
+            }
+
+            fclose(file);
+        }
+    } else {
+        strcpy(msg, "Number must be between 0 and 8\n");
+    }
+
+    sent_msg(msg);
+}
